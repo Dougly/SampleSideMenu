627f0c081e8f866e82bb34183651b9622e2fdc80
Dougly <dgalante11@gmail.com>
2017-06-16 14:29:10 -0400

    table view rows expand

---
+++ SampleSideMenu/UIColorExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIColorExtension.swift
+//  SampleSideMenu
+//
+//  Created by Douglas Galante on 6/16/17.
+//  Copyright © 2017 Dougly. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var themeGreen: UIColor {
+        return UIColor(colorLiteralRed: 0/255, green: 233/255, blue: 145/255, alpha: 1)
+    }
+    
+    static var themeBlue: UIColor {
+        return UIColor(colorLiteralRed: 0/255, green: 193/255, blue: 249/255, alpha: 1)
+    }
+    
+}
--- SampleSideMenu/ViewController.swift
+++ SampleSideMenu/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var sideMenuView: SideMenuView!
     @IBOutlet weak var sideMenuViewTrailingAnchor: NSLayoutConstraint!
     let sampleData = SampleData()
+    var renterIsCollapsed = true
+    var providerIsCollapsed = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,9 +54,60 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SideMenuCell") as! SideMenuCell
         cell.sideMenuCellView.optionLabel.text = sampleData.menuItems[indexPath.row]
+        
+        switch indexPath.row {
+        case 0, 1, 2, 3:
+            cell.sideMenuCellView.contentView.backgroundColor = UIColor.themeBlue
+        case 4, 5, 6, 7:
+            cell.sideMenuCellView.contentView.backgroundColor = UIColor.themeGreen
+        default:
+            break
+        }
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if renterIsCollapsed {
+            switch indexPath.row {
+            case 1, 2, 3: return 0
+            default: break
+            }
+        }
+        
+        if providerIsCollapsed {
+            switch indexPath.row {
+            case 5, 6, 7: return 0
+            default: break
+            }
+        }
+        
+        return 44
+    }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            print("selected renter")
+            if renterIsCollapsed {
+                print("renter was collapsed")
+                renterIsCollapsed = false
+                print("renter is now \(renterIsCollapsed)")
+            } else {
+                renterIsCollapsed = true
+            }
+        case 4:
+            if providerIsCollapsed {
+                providerIsCollapsed = false
+            } else {
+                providerIsCollapsed = true
+            }
+        default:
+            break
+        }
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
     
     
 }
